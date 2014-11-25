62df9766c2902998a2af6e48e90789f79fee325e
Mike <gehaimrat@gmail.com>
2014-11-26 01:16:48 +0300

    heroku version

--- app/services/MongoActor.scala
+++ app/services/MongoActor.scala
@@ -8,46 +8,27 @@ import play.modules.reactivemongo.json.collection.JSONCollection
 import reactivemongo.api.{MongoConnection, MongoDriver}
 import scala.concurrent.ExecutionContext.Implicits.global
 
-
 class MongoActor extends Actor with ActorLogging {
 
   val logger = LoggerFactory.getLogger(this.getClass)
   val conf = ConfigFactory.load()
 
-
-//  val db = {
-//    val driver = new MongoDriver
-//    logger.info("Connecting to " + conf.getString("mongodb.uri"))
-//    val connection: Try[MongoConnection] =
-//      MongoConnection.parseURI(conf.getString("mongodb.uri")).map { parsedUri =>
-//        driver.connection(parsedUri)
-//      }
-//    connection.get
-//  }
-val db = {
-  val driver = new MongoDriver
-  logger.info("Connecting to " + conf.getString("mongo.address"))
-  val connection: MongoConnection = driver.connection(List(conf.getString("mongo.address")))
-  connection(conf.getString("mongo.db"))
-}
+  val db = {
+    val driver = new MongoDriver
+    val uri = MongoConnection.parseURI(System.getenv("MONGOHQ_URL")).get
+    val connection: MongoConnection = driver.connection(uri)
+    connection(uri.db.get)
+  }
   val badCars = db[JSONCollection]("badcars")
 
   override def receive: Receive = {
     //Запрос списка форумов для поиска
-    case cars:List[BadCar] => {
+    case cars: List[BadCar] => {
       logger.debug("Got cars list for insert")
       cars.foreach(badCars.insert(_))
-//      val future = collection.insert(document)
-//
-//      future.onComplete {
-//        case Failure(e) => throw e
-//        case Success(lastError) => {
-//          println("successfully inserted document with lastError = " + lastError)
-//        }
-//      }
     }
 
-    case "Shutdown"	=>
+    case "Shutdown" =>
       log.debug("Mongo actor shutdowns")
       context.stop(self)
       context.system.shutdown()
