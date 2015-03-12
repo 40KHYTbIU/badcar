a17ebc4374b8184fd087302d84f48631d05b8978
Mike <gehaimrat@gmail.com>
2015-03-13 00:26:21 +0300

    Deploy to heroku

--- app/services/HttpActor.scala
+++ app/services/HttpActor.scala
@@ -35,12 +35,12 @@ class HttpActor extends Actor with ActorLogging {
   val spn = "0.402374%2C0.192796"
   val format = "json"
   val geoCodePrefix = "Краснодар,+"
-  val geoCodeUrl = "http://geocode-maps.yandex.ru/1.x/?ll="+ll+"&spn="+spn+"&format="+format+"&results=1&geocode="+geoCodePrefix
-  
+  val geoCodeUrl = "http://geocode-maps.yandex.ru/1.x/?ll=" + ll + "&spn=" + spn + "&format=" + format + "&results=1&geocode=" + geoCodePrefix
+
   //Helpful functions
   def read(url: String): String = io.Source.fromURL(url, "UTF-8").mkString.replaceAll("\n", "")
 
-  def getJsonByUrl(url:String) = Json.parse(read(url))
+  def getJsonByUrl(url: String) = Json.parse(read(url))
 
   def getOnePageJson(page: Int) = getJsonByUrl(URL + "&page=" + page)
 
@@ -53,13 +53,13 @@ class HttpActor extends Actor with ActorLogging {
   }
 
   //Find geo by address
-  def getLocationRedisYandex(addr: String): Location = {    
+  def getLocationRedisYandex(addr: String): Location = {
     val jsonResult = getJsonByUrl(geoCodeUrl + addr.replaceAll("\\s+", "+")) //Without transliteration
     logger.debug("Geo result: " + jsonResult)
     try {
       val position = jsonResult \\ "pos"
       //Take first string with geo, split, reverse and convert to number
-      val Array(lat, lng) = position(0).as[String].split("\\s").reverse.map(x=>x.toDouble)
+      val Array(lat, lng) = position(0).as[String].split("\\s").reverse.map(x => x.toDouble)
       if (lat >= SW_LAT && lat <= NE_LAT && lng >= SW_LNG && lng <= NE_LNG)
         return Location(lat, lng)
       else
@@ -70,24 +70,24 @@ class HttpActor extends Actor with ActorLogging {
     }
     BadGeo
   }
-  
+
   //Heart of actor
-  def getManage(pageNum:Int, pages:Int):Int = {
+  def getManage(pageNum: Int, pages: Int): Int = {
     if (pageNum <= pages) {
       val result = getOnePageJson(pageNum)
       val badCars = (result \ "items").as[Seq[BadCar]]
-      mongoActor ! badCars.map(x => if (x.fromplace.length<5) x.copy(fromplace = "") else x.copy(location = Some(getLocation(x.fromplace)))).toArray
+      mongoActor ! badCars.map(x => if (x.fromplace.length < 5) x.copy(fromplace = "") else x.copy(location = Some(getLocation(x.fromplace)))).toArray
       logger.debug("Result is: " + badCars)
-      getManage(pageNum+1, pages match {case 1=>(result \ "page_count").as[Int] case _ => pages})
+      getManage(pageNum + 1, pages match { case 1 => (result \ "page_count").as[Int] case _ => pages})
     }
     else
       logger.debug("Pages: " + pages)
-    1
+    1 //Need for recursion
   }
 
   def receive = {
     case "get" => mongoActor ! "dropStatus"
-      getManage(1,1)
+      getManage(1, 1)
     case "Shutdown" =>
       logger.debug("Http actor shutdowns")
       context.stop(self)
